1e23c7ad1277f4c4dddb060d2757f8b6d163be8a
Pedro Tersetti Freidinger <pedrotersetti3@gmail.com>
2023-05-13 18:26:52 -0300

    Initial development commit

---
+++ PedroTer7.AspNetBasicAuth/Authentication/IUserCredentialsValidator.cs
@@ -0,0 +1,7 @@
+﻿namespace PedroTer7.AspNetBasicAuth.Authentication
+{
+    public interface IUserCredentialsValidator
+    {
+        Task<bool> Validate(string username, string password);
+    }
+}
