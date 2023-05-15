55c1a7c3d1f20d58a39b1be3b0153bdfadc6ec36
Pedro Tersetti Freidinger <pedrotersetti3@gmail.com>
2023-05-15 18:52:27 -0300

    Add code documentation to public members

--- PedroTer7.AspNetBasicAuth/Extensions/AuthenticationBuilderExtensions.cs
+++ PedroTer7.AspNetBasicAuth/Extensions/AuthenticationBuilderExtensions.cs
@@ -5,6 +5,25 @@ namespace PedroTer7.AspNetBasicAuth.Extensions
 {
     public static class AuthenticationBuilderExtensions
     {
+        /// <summary>
+        /// Enables basic authentication using <see cref="BasicAuthenticationDefaults"/>.
+        /// <para>
+        /// Basic authentication performs authentication by extracting an username and password
+        /// from the Authorize header in the request.
+        /// </para>
+        /// This method registers the needed service implementations in the ASP.NET DI container.
+        /// </summary>
+        /// <typeparam name="T">Data model used by the auth handler to retrieve
+        /// user data after she has been successfully authenticated. This data model
+        /// must b the same as the one used when implementing 
+        /// <see cref="IUserClaimsBuilder{T, K}"/>.</typeparam>
+        /// <typeparam name="K">Schema options used to perform basic authentication
+        /// by the <see cref="BasicAuthenticationHandler{T, K}"/>.</typeparam>
+        /// <param name="builder"></param>
+        /// <param name="referenceType">A type in the assembly where the interfaces needed
+        /// by the basic authentication library are implemented.</param>
+        /// <param name="configureOptions">A delegate to configure the schema options.</param>
+        /// <returns></returns>
         public static AuthenticationBuilder AddBasicAuthentication<T, K>(this AuthenticationBuilder builder, Type referenceType, Action<K> configureOptions)
             where T : new()
             where K : AuthenticationSchemeOptions, new()
@@ -14,6 +33,24 @@ namespace PedroTer7.AspNetBasicAuth.Extensions
             return builder;
         }
 
+        /// <summary>
+        /// Enables basic authentication using <see cref="BasicAuthenticationDefaults"/>
+        /// and <see cref="DefaultBasicAuthenticationSchemeOptions"/> as scheme options.
+        /// <para>
+        /// Basic authentication performs authentication by extracting an username and password
+        /// from the Authorize header in the request.
+        /// </para>
+        /// This method registers the needed service implementations in the ASP.NET DI container.
+        /// </summary>
+        /// <typeparam name="T">Data model used by the auth handler to retrieve
+        /// user data after she has been successfully authenticated. This data model
+        /// must b the same as the one used when implementing 
+        /// <see cref="IUserClaimsBuilder{T, K}"/>.</typeparam>
+        /// <param name="builder"></param>
+        /// <param name="referenceType">A type in the assembly where the interfaces needed
+        /// by the basic authentication library are implemented.</param>
+        /// <param name="configureOptions">A delegate to configure <see cref="DefaultBasicAuthenticationSchemeOptions"/>.</param>
+        /// <returns></returns>
         public static AuthenticationBuilder AddBasicAuthentication<T>(this AuthenticationBuilder builder,
             Type referenceType, Action<DefaultBasicAuthenticationSchemeOptions> configureOptions)
             where T : new()
--- PedroTer7.AspNetBasicAuth/Extensions/SwaggerGenOptionsExtesions.cs
+++ PedroTer7.AspNetBasicAuth/Extensions/SwaggerGenOptionsExtesions.cs
@@ -2,11 +2,22 @@
 using Microsoft.OpenApi.Models;
 using PedroTer7.AspNetBasicAuth.Documentation;
 using Swashbuckle.AspNetCore.SwaggerGen;
+using PedroTer7.AspNetBasicAuth.Authentication;
 
 namespace PedroTer7.AspNetBasicAuth.Extensions
 {
     public static class SwaggerGenOptionsExtesions
     {
+        /// <summary>
+        /// Configures basic authentication schema documentation 
+        /// in the generated OpenAPI document.
+        /// <para>
+        /// This overload applies the basic authentication schema
+        /// for every action containing at least one <c>Authorize</c>
+        /// attribute. See other overloads to fine grain this behaviour.
+        /// </para>
+        /// </summary>
+        /// <param name="options"></param>
         public static void AddBasicAuthentication(this SwaggerGenOptions options)
         {
             AddSecurityDefinition(options);
@@ -14,6 +25,34 @@ namespace PedroTer7.AspNetBasicAuth.Extensions
             RegisterOperationFilter(options, filterSettings);
         }
 
+        /// <summary>
+        /// Configures basic authentication schema documentation 
+        /// in the generated OpenAPI document.
+        /// <para>
+        /// If both <paramref name="policies"/> and <paramref name="roles"/> are
+        /// <c>null</c> and <paramref name="everyAuthorizeAttribute"/> is
+        /// <c>false</c>, then the method will apply the authentication
+        /// schema to every action that contains an <c>Authorize</c> attribute
+        /// that has <see cref="BasicAuthenticationDefaults.SchemaName"/>
+        /// in its <c>AuthenticationSchemes</c> list.
+        /// </para>
+        /// </summary>
+        /// <param name="options"></param>
+        /// <param name="everyAuthorizeAttribute">If <c>true</c> then
+        /// the basic authentication schema will be applied to every
+        /// action that contains at least one <c>Authorize</c> attribute.</param>
+        /// <param name="policies">A list of policies that the operation will use
+        /// to decide if the authentication schema should be applied to a given action
+        /// that contains <c>Authorize</c> attribute(s). If a given action contains
+        /// an <c>Authorize</c> attribute with a policy that is within this list
+        /// then the schema will be applied to that method. Policies take precedence
+        /// over roles if both are given.</param>
+        /// <param name="roles">A list of roles that the operation will use
+        /// to decide if the authentication schema should be applied to a given action
+        /// that contains <c>Authorize</c> attribute(s). If a given action contains
+        /// an <c>Authorize</c> attribute and at least one of the roles in the attribute
+        /// roles array is within this list then the schema is applied to that method.
+        /// Policies take precedence over roles if both are given.</param>
         public static void AddBasicAuthentication(this SwaggerGenOptions options,
             bool everyAuthorizeAttribute, string[]? policies = null, string[]? roles = null)
         {
